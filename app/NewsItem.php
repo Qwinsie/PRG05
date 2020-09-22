78dd8784ba6e1a93e2861cbd07c5864d627fc537
Qwinsie <qvdsluis@gmail.com>
2020-09-22 15:12:05 +0200

    Edited the NewsItem.php

---
+++ app/NewsItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\NewsItem
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|NewsItem query()
+ * @mixin \Eloquent
+ */
+class NewsItem extends Model
+{
+
+//    public $fillable = ['title', 'description', 'image', 'category'];
+//
+//    public function category()
+//    {
+//        return $this->Belongsto
+//    }
+}
